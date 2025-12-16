839d6ecdfe883e626f74d8057295864fba86e170
Sirapop Chanphui <srp073536@gmail.com>
2025-12-16 13:37:15 +0700

    refactor: update salary and score functions to improve logic and clarity

--- ex-1.js
+++ ex-1.js
@@ -3,15 +3,22 @@ const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 
 function addSalary5000(previousSalary) {
   // Start coding here
+  return previousSalary + 5000;
 }
 
 function forEach(array, operation) {
   // Start coding here
+  let newEmployeeSalaries = [];
+  for (let employeeSalary of array) {
+    newEmployeeSalaries.push(operation(employeeSalary));
+  }
+
+  return newEmployeeSalaries;
 }
 
 // Using `forEach` function here
 
-let newEmployeeSalaries;
+let newEmployeeSalaries = forEach(employeeSalaries, addSalary5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 
@@ -19,8 +26,10 @@ console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+Ans addSalary5000 
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+Ans forEach 
 
 ====================================
 */
